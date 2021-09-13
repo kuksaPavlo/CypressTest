babac245d4d6567754c8b15b6162af6e3f25aa43
Perenio1 <perenio1@MBP-2372.lan>
2021-09-13 16:43:50 +0300

    Assertions

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,24 +1,46 @@
 ///<reference types="Cypress" />
 
-
-it('', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-   cy.contains('Sign in'); 
+it('SHOULD', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100)
+        .should('have.value', 100)
+        .end('be.visible')
 })
 
-it('', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.contains('div', 'Sign in');
+it('EXPECT', () => {
+    cy.visit('https://next.privat24.ua/mobile/?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100).then( input => {
+            expect(input).to.have.value(100)
+        })
 })
 
-it('', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.contains('SIGN IN', {matchCase: false}); //Ignore upercase
+it('check default value for Deposits', () => {
+    cy.visit('https://next.privat24.ua/deposit/?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+    .should('be.checked')
 })
 
-it.only('', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('footer').contains('Regulations and tariffs')
+it('check is visible on hover', () => {
+    cy.visit('https://next.privat24.ua/deposit/?lang=en')
+    cy.contains('Мої депозити')
+        .trigger('mouseover')
+        .get('#archiveDeposits')
+        .should('be.visible')
+
 })
 
-//hghg
+it('Check is correct attr in a button', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+       .should('have.attr', 'type')
+       .and('match', /button/) 
+})
+
+it.only('Check is correct URL', () => {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+       .should('eq', 'https://next.privat24.ua/?lang=en')
+    
+})
